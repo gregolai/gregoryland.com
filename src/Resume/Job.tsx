605818abe7616520cb6d3a3896e8806a2aae1a9a
Gregory Dalton <gregolai@gmail.com>
2020-03-19 00:34:19 -0400

    p

--- src/Resume/Job.tsx
+++ src/Resume/Job.tsx
@@ -6,7 +6,26 @@ export const Job = ({ children, where, role, from, to, css = {} }) => (
 	<Box css={{ pt: space._6, ...css }}>
 		<Box>
 			<Flex css={{ alignItems: 'center' }}>
-				<Text.Subtitle css={{ textTransform: 'uppercase' }}>{role}</Text.Subtitle>
+				<Text.Subtitle
+					css={{
+						color: 'transparent',
+						// firefox
+						mozTextStrokeWidth: '1px',
+						mozTextStrokeColor: 'black',
+
+						// chrome/safari
+						webkitTextStrokeWidth: '1px',
+						webkitTextStrokeColor: 'black',
+
+						textTransform: 'uppercase',
+
+						fontSize: '32px',
+						lineHeight: '32px',
+						letterSpacing: '2px'
+					}}
+				>
+					{role}
+				</Text.Subtitle>
 
 				<Text.Subtitle
 					css={{ flex: '1', textAlign: 'right', fontWeight: '500', textTransform: 'uppercase' }}
