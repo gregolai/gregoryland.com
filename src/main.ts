7b41eecffa463cecdc74f504b812cfe2944fac1b
Gregory Dalton <gregolai@gmail.com>
2020-04-30 01:01:52 -0400

    p

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,9 @@ import { createElement } from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-require('./main.scss');
-
 const ua = window.navigator.userAgent;
 if (ua.indexOf('MSIE ') > 0 || ua.indexOf('Trident/') > 0 /* || ua.indexOf('Edge/') > 0*/) {
-	const el = document.createElement('b');
-	el.innerText = 'IE bad.';
-	document.body.appendChild(el);
-	setTimeout(() => (window.location.href = 'https://www.google.com/chrome/'), 1000);
+	window.location.href = 'https://www.google.com/chrome/';
 } else {
-	render(createElement(App, {}), document.getElementById('app'));
+	render(createElement(App), document.getElementById('app'));
 }
