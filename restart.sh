64e0bb34ec6d7cd8368cf5165d8a1bbd1e62237a
Gregory Dalton <gregolai.gmail.com>
2021-08-04 17:12:14 +0000

    p

--- restart.sh
+++ restart.sh
@@ -5,4 +5,4 @@ npm run build:prod
 
 pm2 stop --silent new.gregoryland.com
 pm2 delete --silent new.gregoryland.com
-pm2 start "npx serve public -l 8080" --name new.gregoryland.com
+pm2 start "npx serve public -l 80" --name new.gregoryland.com
