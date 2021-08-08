e1b41605ad221594820ca088b7d678ebf0dedd90
Gregory Dalton <gregolai@gmail.com>
2021-08-08 03:38:52 -0500

    p

--- restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull
-npm i
-npm run build:prod
-
-pm2 stop --silent new.gregoryland.com
-pm2 delete --silent new.gregoryland.com
-pm2 start "npx serve public -l 80" --name new.gregoryland.com
