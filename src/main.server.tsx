5e23f14f939ed8c14ae321e346c756b5ba754d80
Gregory Dalton <gregolai@gmail.com>
2022-03-01 14:47:53 -0600

    add google analytics

--- src/main.server.tsx
+++ src/main.server.tsx
@@ -51,11 +51,27 @@ const renderSSR = ({ appHtml, styleHtml }: any) => `
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 	<title>Gregory Dalton</title>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TD3HVV5');</script>
+	<!-- End Google Tag Manager -->
+
 	<style>${cssReset}</style>
 	${styleHtml}
 	<script defer type="text/javascript" src="${manifest['main.client.tsx']}"></script>
 </head>
-<body><div id="root">${appHtml}</div></body>
+<body>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD3HVV5"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	
+	<div id="root">${appHtml}</div>
+</body>
 </html>
 `;
 
